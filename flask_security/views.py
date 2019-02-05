e7725e038a73b0889ac6f89520fd65a8f590c0ee
Antonio Rodrigo dos Santos Silva <20875150+rodrigosantosbr@users.noreply.github.com>
2019-02-05 11:42:11 -0300

    Update views.py

--- flask_security/views.py
+++ flask_security/views.py
@@ -77,7 +77,8 @@ def login():
     if form.validate_on_submit():
         try:
             login_user(form.email, remember=form.remember.data)
-        except:
+        except Exception as e:
+            print(e)
             login_user(form.user, remember=form.remember.data)
         after_this_request(_commit)
 
